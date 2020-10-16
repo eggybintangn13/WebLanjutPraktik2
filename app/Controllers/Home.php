3f90e3b0e05e722bc7250257d269313af298f0aa
eggybintangn13 <eggybintangn@gmail.com>
2020-10-16 17:05:32 +0700

    Pertemuian 3

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,16 @@ class Home extends BaseController
 	{
 		return view('welcome_message');
 	}
-
 	//--------------------------------------------------------------------
+
+	public function show()
+	{
+		$data['nama'] = 'Eggy';
+		$data['jurusan'] = 'Ilkomp';
+		$data['npm'] = '1817051013';
+		echo view('mahasiswa/header', $data);
+		echo view('mahasiswa/index', $data);
+		echo view('mahasiswa/footer', $data);
+	}
 
 }
